42390d9d45a19653e6e54a29372757f22b4b3a3d
Feras B A Y R Alshadad <83323@kfh.com>
2024-01-23 20:14:24 +0300

    Updated the repo

--- app/src/main/java/com/counter/taskandroidaccountstatementslist/repo/TransactionRepo.kt
+++ app/src/main/java/com/counter/taskandroidaccountstatementslist/repo/TransactionRepo.kt
@@ -8,6 +8,8 @@ class TransactionRepo {
         Transaction("Saving", 245.23, 4354.8, "KWD", "Withdrawal"),
         Transaction("Salary", 575.23, 3465.4, "KWD", "Deposit"),
         Transaction("Invest", 3566.23, 554354.8, "KWD", "Deposit"),
+         Transaction("Salary", 366.23, 74354.8, "KWD", "Withdrawal"),
+         Transaction("Saving", 3916.23, 44354.8, "KWD", "Deposit"),
     )
    }
 }
